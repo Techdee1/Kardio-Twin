9b181b596b48c6baff4f26af21c0a9c4ebea7c16
Akeem Jr Odebiyi <160186292+Techdee1@users.noreply.github.com>
2026-02-22 06:34:21 +0000

    Fix CORS: allow_credentials=False for wildcard origins

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ app = FastAPI(title="CardioTwin API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
